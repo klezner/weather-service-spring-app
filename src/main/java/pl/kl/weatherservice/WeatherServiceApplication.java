69fae6a146adfc779fdc0b0be439bb26b4d140c2
Krzysztof Lezner <krzys3ek@gmail.com>
2021-02-24 10:19:19 +0100

    Initial commit

---
+++ src/main/java/pl/kl/weatherservice/WeatherServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.kl.weatherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherServiceApplication.class, args);
+    }
+
+}
