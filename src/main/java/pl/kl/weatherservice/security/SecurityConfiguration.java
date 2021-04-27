f15f9922293d4aca7f5dee62c8f6a2ae802537ef
Krzysztof Lezner <krzys3ek@gmail.com>
2021-04-27 10:01:23 +0200

    Fix login bug

--- src/main/java/pl/kl/weatherservice/security/SecurityConfiguration.java
+++ src/main/java/pl/kl/weatherservice/security/SecurityConfiguration.java
@@ -20,6 +20,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .httpBasic()
+                .and()
                 .formLogin()
                 .and()
                 .logout()
